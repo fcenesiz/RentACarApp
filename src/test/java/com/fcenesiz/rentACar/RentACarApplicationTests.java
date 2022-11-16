b1144117c936774acf5ae5f5db72cdeb31d8643a
Fatih <ahmetfatihcenesizdev@gmail.com>
2022-11-16 13:23:08 +0300

    Initial commit

---
+++ src/test/java/com/fcenesiz/rentACar/RentACarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcenesiz.rentACar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentACarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
